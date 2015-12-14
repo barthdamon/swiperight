c613fdaccd61c1e66adf8da3731ec3c4a1b525c6
MattxDamon <mbmattbarth@gmail.com>
2015-12-14 16:54:55 -0500

    transfers game view code to game view, about to start using it

--- SwipeRight/GameView.swift
+++ SwipeRight/GameView.swift
@@ -8,14 +8,187 @@
 
 import UIKit
 
-class GameView: UIView {
+protocol GameViewDelegate {
+  func beginGame()
+  func scoreChange(correct: Bool)
+}
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
+class GameView: UIView {
+  
+  var delegate: GameViewDelegate!
+  var tileWidth: CGFloat!
+  var viewWidth: CGFloat!
+  var tileViews: Array<TileView> = []
+  
+  var startLoc: CGPoint?
+  var endLoc: CGPoint?
+  
+  var currentLayout: GridNumberLayout?
+  var nextLayout: GridNumberLayout?
+  
+  convenience init(viewWidth: CGFloat, delegate: GameViewDelegate) {
+    self.init()
+    self.viewWidth = viewWidth
+    
+    let offset = (viewWidth - (viewWidth / 1.25)) / 2
+    self.frame = CGRectMake(offset, 210, viewWidth / 1.25, viewWidth / 1.25)
+    // KEEP return this as var: gameViewWidth = gameView!.frame.width
+    //also need tileWidth to be a var based off this view
+    tileWidth = self.frame.width / 3
+    self.backgroundColor = UIColor.darkGrayColor()
+    self.userInteractionEnabled = false
+    // KEEP return the var self.view.addSubview(gameView!)
+    addGestureRecognizers()
+    configureGameViewComponents()
+  }
+
+  func addGestureRecognizers() {
+    let swipeRecognizer = UIPanGestureRecognizer(target: self, action: "gameViewSwiped:")
+    swipeRecognizer.minimumNumberOfTouches = 1
+    self.addGestureRecognizer(swipeRecognizer)
+  }
+  
+  func configureGameViewComponents() {
+    var coords = Coordinates(x: 0, y: 0)
+    
+    func adjustCoords(i: Int) {
+      coords.x = coords.x + tileWidth
+      if i == 2 || i == 5  {
+        coords.x = 0
+        coords.y = coords.y + tileWidth
+      }
     }
-    */
+    
+    for var i = 0; i < 9; i++ {
+      let tileView = TileView(xCoord: coords.x, yCoord: coords.y, tileWidth: tileWidth, overlay: false)
+      tileViews.append(tileView)
+      adjustCoords(i)
+      self.addSubview(tileView)
+    }
+  }
+  
+  func gameViewSwiped(sender: UIGestureRecognizer) {
+    let loc = sender.locationInView(self)
+    if sender.state == .Began {
+      startLoc = loc
+      print("Gesture Began: \(loc)")
+      resolveUserInteraction()
+    }
+    if sender.state == .Ended {
+      endLoc = loc
+      print("Gesture Ended \(loc)")
+      resolveUserInteraction()
+    }
+  }
+  
+  func resolveUserInteraction() {
+    if let startLoc = startLoc, endLoc = endLoc {
+      var startTile: TileView?
+      var endTile: TileView?
+      var midTile: TileView?
+      //Int floors the cgfloat
+      let start = (x: Int(startLoc.x / tileWidth), y: Int(startLoc.y / tileWidth))
+      print("START: \(start.x), \(start.y)")
+      let end = (x: Int(endLoc.x / tileWidth), y: Int(endLoc.y / tileWidth))
+      print("END: \(end.x), \(end.y)")
+      let mid = (x: (start.x + end.x) / 2, y: (start.y + end.y) / 2)
+      print("MIDDLE: \(mid.x), \(mid.y)")
+      
+      for var i = 0; i < Grid.tileCoordinates.count; i++ {
+        let loc = Grid.tileCoordinates[i]
+        if loc.x == start.x && loc.y == start.y {
+          startTile = tileViews[i]
+          print("START TILE FOUND: \(i)")
+        }
+        if loc.x == end.x && loc.y == end.y {
+          endTile = tileViews[i]
+          print("END TILE FOUND: \(i)")
+        }
+        if loc.x == mid.x && loc.y == mid.y {
+          midTile = tileViews[i]
+          print("MID TILE FOUND: \(i)")
+        }
+      }
+      
+      if let startTile = startTile, midTile = midTile, endTile = endTile {
+        tileRespond(startTile, middleTile: midTile, endTile: endTile)
+        self.startLoc = nil
+        self.endLoc = nil
+      }
+    }
+  }
+  
+  func tileRespond(startTile: TileView, middleTile: TileView, endTile: TileView) {
+    print("TILE RESPOND TIME")
+    if let startNumber = startTile.number, midNumber = middleTile.number, endNumber = endTile.number {
+      if startNumber + midNumber == endNumber {
+        delegate.scoreChange(true)
+        startTile.backgroundColor = UIColor.greenColor()
+        endTile.backgroundColor = UIColor.greenColor()
+        middleTile.backgroundColor = UIColor.greenColor()
+        self.userInteractionEnabled = false
+        resetTiles()
+      } else {
+        delegate.scoreChange(false)
+        startTile.backgroundColor = UIColor.redColor()
+        endTile.backgroundColor = UIColor.redColor()
+        middleTile.backgroundColor = UIColor.redColor()
+        self.userInteractionEnabled = false
+        resetTiles()
+      }
+    }
+  }
+  
+  func resetTiles() {
+    if let nextLayout = self.nextLayout {
+      self.currentLayout = nextLayout
+      self.nextLayout = GridNumberLayout()
+    } else {
+      self.currentLayout = GridNumberLayout()
+      //in the future generate next layout asynchronously in thebackground after current layout is generated
+      self.nextLayout = GridNumberLayout()
+    }
+    animateTileReset()
+  }
+  
+  func animateTileReset() {
+    func fadeInTiles() {
+      tileViews.forEach { (tile) -> () in
+        UIView.animateWithDuration(0.2, animations: { () -> Void in
+          tile.numberLabel?.alpha = 1
+          }, completion: { (complete) -> Void in
+            self.userInteractionEnabled = true
+            self.delegate.beginGame()
+        })
+      }
+    }
+    
+    tileViews.forEach { (tile) -> () in
+      UIView.animateWithDuration(0.2, animations: { () -> Void in
+        tile.backgroundColor = UIColor.cyanColor()
+        tile.numberLabel?.alpha = 0
+        }, completion: { (complete) -> Void in
+          self.applyNumberLayoutToTiles(false)
+          fadeInTiles()
+      })
+    }
+  }
+  
+  func applyNumberLayoutToTiles(reset: Bool) {
+    if let layout = currentLayout {
+      for var i = 0; i < layout.numbers.count; i++ {
+        if reset {
+          tileViews[i].numberLabel?.alpha = 0
+        } else {
+          tileViews[i].numberLabel?.alpha = 1
+          tileViews[i].number = layout.numbers[i]
+        }
+      }
+    }
+  }
+
+
+
+
 
 }
