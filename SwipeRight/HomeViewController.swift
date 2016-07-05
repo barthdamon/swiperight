4c7a4856f6a65a788461f27b28e738d0b44b28ee
MattxDamon <mbmattbarth@gmail.com>
2016-07-05 18:12:46 -0400

    reimplements leaderboards... not finding bugs

--- SwipeRight/HomeViewController.swift
+++ SwipeRight/HomeViewController.swift
@@ -15,7 +15,7 @@ class HomeViewController: UIViewController, ButtonDelegate, GKGameCenterControll
   @IBOutlet weak var logoView: UIImageView!
   @IBOutlet weak var firstTimeButton: UIButton!
   @IBOutlet weak var beginGameButtonView: ButtonView!
-//  @IBOutlet weak var leaderboardsButton: UIButton!
+  @IBOutlet weak var leaderboardsButton: UIButton!
   @IBOutlet weak var removeAdsButton: UIButton!
   
   @IBOutlet weak var beginGameLabel: UILabel!
@@ -42,7 +42,7 @@ class HomeViewController: UIViewController, ButtonDelegate, GKGameCenterControll
     beginGameButtonView.alpha = 0
     firstTimeButton.alpha = 0
     logoView.alpha = 0
-//    leaderboardsButton.alpha = 0
+    leaderboardsButton.alpha = 0
     removeAdsButton.alpha = 0
     if GameStatus.status.gc_enabled {
       authenticateLocalPlayer()
@@ -56,7 +56,7 @@ class HomeViewController: UIViewController, ButtonDelegate, GKGameCenterControll
       self.beginGameButtonView.alpha = 1
       self.firstTimeButton.alpha = 1
       self.logoView.alpha = 1
-//      self.leaderboardsButton.alpha = 1
+      self.leaderboardsButton.alpha = 1
       self.removeAdsButton.alpha = 1
     }
   }
